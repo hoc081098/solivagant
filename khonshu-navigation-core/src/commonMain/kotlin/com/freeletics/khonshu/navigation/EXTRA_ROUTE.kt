6bc4026f9f3b222073930bb82361a2b8370195b3
hoc081098@gmail.com <hoc081098@gmail.com>
2023-12-30 16:36:33 +0700

    khonshu: add navigation module - done ActivityRoute.kt

---
+++ khonshu-navigation-core/src/commonMain/kotlin/com/freeletics/khonshu/navigation/EXTRA_ROUTE.kt
@@ -0,0 +1,6 @@
+package com.freeletics.khonshu.navigation
+
+import com.freeletics.khonshu.navigation.internal.InternalNavigationApi
+
+@InternalNavigationApi
+public const val EXTRA_ROUTE: String = "com.freeletics.khonshu.navigation.ROUTE"
