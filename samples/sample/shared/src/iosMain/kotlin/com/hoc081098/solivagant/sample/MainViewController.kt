abf147e1c0464eeaaf94b50e4341053800a728dd
hoc081098@gmail.com <hoc081098@gmail.com>
2024-01-27 23:15:34 +0700

    fix lifecycle ios

--- samples/sample/shared/src/iosMain/kotlin/com/hoc081098/solivagant/sample/MainViewController.kt
+++ samples/sample/shared/src/iosMain/kotlin/com/hoc081098/solivagant/sample/MainViewController.kt
@@ -1,7 +1,22 @@
 package com.hoc081098.solivagant.sample
 
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.window.ComposeUIViewController
+import com.hoc081098.solivagant.lifecycle.LocalLifecycleOwner
+import com.hoc081098.solivagant.navigation.internal.AppLifecycleOwner
+import com.hoc081098.solivagant.sample.common.OnLifecycleEventWithBuilder
+import io.github.aakira.napier.Napier
 import platform.UIKit.UIViewController
 
-@Suppress("FunctionName")
-fun MainViewController(): UIViewController = ComposeUIViewController { SolivagantSampleApp() }
+internal val AppLifecycleOwner by lazy { AppLifecycleOwner() }
+
+@Suppress("FunctionName", "unused")
+fun MainViewController(): UIViewController = ComposeUIViewController {
+  CompositionLocalProvider(LocalLifecycleOwner provides AppLifecycleOwner) {
+    OnLifecycleEventWithBuilder {
+      onEach { Napier.d(message = "Lifecycle event: $it", tag = "[main]") }
+    }
+
+    SolivagantSampleApp()
+  }
+}
