18a19d67923e5fb7ac907b6e593485409ab63d40
hoc081098@gmail.com <hoc081098@gmail.com>
2024-01-27 14:49:15 +0700

    add ios sample

---
+++ samples/sample/shared/src/nativeMain/kotlin/com/hoc081098/solivagant/sample/common/platformToast.native.kt
@@ -0,0 +1,9 @@
+package com.hoc081098.solivagant.sample.common
+
+import androidx.compose.runtime.Stable
+
+@Stable
+actual class PlatformToastManager actual constructor() {
+  actual fun showToast(message: String) {
+  }
+}
