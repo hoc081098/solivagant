3ccad91012d68980de03b6ba2716ea850594a52c
hoc081098@gmail.com <hoc081098@gmail.com>
2024-01-31 18:55:34 +0700

    baseName = "SolivagantSimpleSampleAppShared"

--- samples/simple/shared/build.gradle.kts
+++ samples/simple/shared/build.gradle.kts
@@ -43,7 +43,7 @@ kotlin {
     iosSimulatorArm64(),
   ).forEach { iosTarget ->
     iosTarget.binaries.framework {
-      baseName = "SolivagantSampleAppShared"
+      baseName = "SolivagantSimpleSampleAppShared"
       isStatic = true
     }
   }
