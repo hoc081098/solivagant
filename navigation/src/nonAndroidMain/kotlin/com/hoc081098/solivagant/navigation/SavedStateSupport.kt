deba8cfd89a73fa67cd965adc20e91a7c852153e
hoc081098@gmail.com <hoc081098@gmail.com>
2024-02-04 14:09:58 +0700

    docs: SavedStateSupport

--- navigation/src/nonAndroidMain/kotlin/com/hoc081098/solivagant/navigation/SavedStateSupport.kt
+++ navigation/src/nonAndroidMain/kotlin/com/hoc081098/solivagant/navigation/SavedStateSupport.kt
@@ -8,6 +8,48 @@ import com.hoc081098.kmp.viewmodel.ViewModelStore
 import com.hoc081098.kmp.viewmodel.ViewModelStoreOwner
 import kotlin.LazyThreadSafetyMode.NONE
 
+/**
+ * A class helps to support saved state in Compose.
+ *
+ * The [clear] method will clear all saved state and clear the [ViewModelStore].
+ * Usually, you should call [clear] in `onDispose` of a [androidx.compose.runtime.DisposableEffect]
+ * that runs in the root @Composable.
+ *
+ * The [performSave] method will save all the saved state.
+ * Usually, you should call [performSave] in `onDispose` of a [androidx.compose.runtime.DisposableEffect]
+ * that runs in a child @Composable.
+ * That [androidx.compose.runtime.DisposableEffect] should be at the last of the child @Composable,
+ * because [SaveableStateRegistry.Entry]s are unregistered in reverse order.
+ * We want to save state first, before [SaveableStateRegistry.Entry]s are unregistered.
+ *
+ * ## Example
+ *
+ * ```kotlin
+ * // Remember a SavedStateSupport instance.
+ * val savedStateSupport = remember { SavedStateSupport() }
+ *
+ * // Clear the SavedStateSupport when the root @Composable exits the composition.
+ * DisposableEffect(Unit) {
+ *   onDispose(savedStateSupport::clear)
+ * }
+ *
+ * // Provide SavedStateSupport as ViewModelStoreOwner, SaveableStateRegistry and SavedStateHandleFactory.
+ * CompositionLocalProvider(
+ *   LocalViewModelStoreOwner provides savedStateSupport,
+ *   LocalSaveableStateRegistry provides savedStateSupport,
+ *   LocalSavedStateHandleFactory provides savedStateSupport,
+ * ) {
+ *   MyApp()
+ *
+ *   // Must be at the last,
+ *   // because onDispose is called in reverse order, so we want to save state first,
+ *   // before [SaveableStateRegistry.Entry]s are unregistered.
+ *   DisposableEffect(Unit) {
+ *     onDispose(savedStateSupport::performSave)
+ *   }
+ * }
+ * ```
+ */
 @MainThread
 public class SavedStateSupport :
   SavedStateHandleFactory,
