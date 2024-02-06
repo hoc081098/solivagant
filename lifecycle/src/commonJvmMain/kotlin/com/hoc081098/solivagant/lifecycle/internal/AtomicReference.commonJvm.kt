6a9e93b6b040fb6d57bb8631b2217478851f5253
hoc081098@gmail.com <hoc081098@gmail.com>
2024-02-06 17:29:49 +0700

    fully qualified import for WeakReference https://github.com/Kotlin/dokka/issues/3403

--- lifecycle/src/commonJvmMain/kotlin/com/hoc081098/solivagant/lifecycle/internal/AtomicReference.commonJvm.kt
+++ lifecycle/src/commonJvmMain/kotlin/com/hoc081098/solivagant/lifecycle/internal/AtomicReference.commonJvm.kt
@@ -1,7 +1,5 @@
 package com.hoc081098.solivagant.lifecycle.internal
 
-import java.util.concurrent.atomic.AtomicReference
-
 // TODO: https://youtrack.jetbrains.com/issue/KT-37316
 @Suppress("ACTUAL_WITHOUT_EXPECT") // internal expect is not matched with internal typealias to public type
-internal actual typealias AtomicReference<T> = AtomicReference<T>
+internal actual typealias AtomicReference<T> = java.util.concurrent.atomic.AtomicReference<T>
