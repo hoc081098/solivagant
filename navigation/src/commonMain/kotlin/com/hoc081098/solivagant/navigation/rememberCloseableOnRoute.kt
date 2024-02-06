399460929a117f314aa994bedd836cee2ba6e80a
hoc081098@gmail.com <hoc081098@gmail.com>
2024-02-06 15:39:11 +0700

    add rememberCloseableOnRoute

--- navigation/src/commonMain/kotlin/com/hoc081098/solivagant/navigation/rememberCloseableOnRoute.kt
+++ navigation/src/commonMain/kotlin/com/hoc081098/solivagant/navigation/rememberCloseableOnRoute.kt
@@ -9,11 +9,17 @@ import com.hoc081098.kmp.viewmodel.Closeable
 import com.hoc081098.solivagant.navigation.internal.destinationId
 import kotlin.reflect.KClass
 
+/**
+ * Remember a [Closeable] on the route.
+ *
+ * The [Closeable] will be created by [factory] once at the first access.
+ * It will be closed when the [route] is removed from the back stack.
+ */
 @Composable
 public fun <T : Closeable> rememberCloseableOnRoute(
   route: BaseRoute,
   type: KClass<T>,
-  factory: @DisallowComposableCalls () -> T,
+  factory: () -> T,
 ): T {
   val executor = LocalNavigationExecutor.current
   val currentFactory by rememberUpdatedState(factory)
@@ -27,6 +33,12 @@ public fun <T : Closeable> rememberCloseableOnRoute(
   }
 }
 
+/**
+ * Remember a [Closeable] on the route.
+ *
+ * The [Closeable] will be created by [factory] once at the first access.
+ * It will be closed when the [route] is removed from the back stack.
+ */
 @Composable
 public inline fun <reified T : Closeable> rememberCloseableOnRoute(
   route: BaseRoute,
