601fd1a42cb23d2cacf015cc79f0d8de161aa2a0
hoc081098@gmail.com <hoc081098@gmail.com>
2024-02-07 11:29:01 +0700

    license

--- navigation/src/jsMain/kotlin/com/hoc081098/solivagant/navigation/internal/rememberPlatformLifecycleOwner.js.kt
+++ navigation/src/jsMain/kotlin/com/hoc081098/solivagant/navigation/internal/rememberPlatformLifecycleOwner.js.kt
@@ -17,16 +17,13 @@
 package com.hoc081098.solivagant.navigation.internal
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import com.hoc081098.solivagant.lifecycle.Lifecycle
 import com.hoc081098.solivagant.lifecycle.LifecycleOwner
 import com.hoc081098.solivagant.lifecycle.LifecycleRegistry
 
-private class DefaultJsLifecycleOwner : LifecycleOwner {
+private object DefaultJsLifecycleOwner : LifecycleOwner {
   override val lifecycle: Lifecycle = LifecycleRegistry(initialState = Lifecycle.State.RESUMED)
 }
 
 @Composable
-internal actual fun rememberPlatformLifecycleOwner(): LifecycleOwner {
-  return remember { DefaultJsLifecycleOwner() }
-}
+internal actual fun rememberPlatformLifecycleOwner(): LifecycleOwner = DefaultJsLifecycleOwner
