c7aaba967992401a0fc55db596ec277c26270d7e
hoc081098@gmail.com <hoc081098@gmail.com>
2024-02-17 19:18:40 +0700

    samples/simple: add Koin's `onClose` callback to `DetailNavigator`

--- samples/simple/shared/src/commonMain/kotlin/com/hoc081098/solivagant/sample/simple/ui/detail/DetailNavigator.kt
+++ samples/simple/shared/src/commonMain/kotlin/com/hoc081098/solivagant/sample/simple/ui/detail/DetailNavigator.kt
@@ -1,5 +1,6 @@
 package com.hoc081098.solivagant.sample.simple.ui.detail
 
+import com.hoc081098.kmp.viewmodel.Closeable
 import com.hoc081098.kmp.viewmodel.parcelable.Parcelable
 import com.hoc081098.kmp.viewmodel.parcelable.Parcelize
 import com.hoc081098.solivagant.navigation.NavEventNavigator
@@ -9,7 +10,11 @@ import kotlinx.coroutines.flow.Flow
 @Parcelize
 data class DetailScreenResult(val value: String) : Parcelable
 
-class DetailNavigator : NavEventNavigator() {
+/**
+ * This class is used by DetailScreen and DetailScreenOverlay.
+ * It is scoped to [DetailScreenRoute].
+ */
+class DetailNavigator : NavEventNavigator(), Closeable {
   init {
     Napier.d(message = "$this::init")
   }
@@ -20,5 +25,9 @@ class DetailNavigator : NavEventNavigator() {
 
   fun deliverResultToDetailScreen(value: String) {
     deliverNavigationResult(detailScreenResultRequest.key, DetailScreenResult(value))
+  }
+
+  override fun close() {
+    Napier.d(message = "$this::close")
   }
 }
--- samples/simple/shared/src/commonMain/kotlin/com/hoc081098/solivagant/sample/simple/ui/detail/di.kt
+++ samples/simple/shared/src/commonMain/kotlin/com/hoc081098/solivagant/sample/simple/ui/detail/di.kt
@@ -2,10 +2,11 @@ package com.hoc081098.solivagant.sample.simple.ui.detail
 
 import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
+import org.koin.dsl.onClose
 
 val DetailModule = module {
   scope<DetailScreenRoute> {
-    scoped { DetailNavigator() }
+    scoped { DetailNavigator() }.onClose { it?.close() }
     factoryOf(::DetailViewModel)
   }
 }
