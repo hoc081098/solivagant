7101be3ac436f6f2e3b37a7946a8289278b4e9a6
hoc081098@gmail.com <hoc081098@gmail.com>
2024-03-03 17:50:56 +0700

    add wasm support

--- samples/solivagant-wasm-sample/src/wasmJsMain/kotlin/com/hoc081098/solivagant/sample/wasm/second/SecondScreenRoute.kt
+++ samples/solivagant-wasm-sample/src/wasmJsMain/kotlin/com/hoc081098/solivagant/sample/wasm/second/SecondScreenRoute.kt
@@ -1,11 +1,12 @@
 package com.hoc081098.solivagant.sample.wasm.second
 
-import androidx.compose.material3.Text
 import com.hoc081098.solivagant.navigation.NavRoute
 import com.hoc081098.solivagant.navigation.ScreenDestination
 
 data object SecondScreenRoute : NavRoute
 
 val SecondScreenDestination = ScreenDestination<SecondScreenRoute> { _, modifier ->
-  Text("Second")
+  SecondScreen(
+    modifier = modifier,
+  )
 }
