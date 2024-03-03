7ac940f0f92e8a4b852f829848be2d1e8ae82f80
hoc081098@gmail.com <hoc081098@gmail.com>
2024-03-03 16:39:31 +0700

    add wasm sample

---
+++ samples/solivagant-wasm-sample/build.gradle.kts
@@ -0,0 +1,41 @@
+import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
+import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
+
+plugins {
+  alias(libs.plugins.kotlin.multiplatform)
+  alias(libs.plugins.jetbrains.compose)
+}
+
+kotlin {
+  @OptIn(ExperimentalWasmDsl::class)
+  wasmJs {
+    moduleName = "composeApp"
+    browser {
+      commonWebpackConfig {
+        outputFileName = "composeApp.js"
+        devServer = (devServer ?: KotlinWebpackConfig.DevServer()).apply {
+          static = (static ?: mutableListOf()).apply {
+            // Serve sources to debug inside browser
+            add(project.projectDir.path)
+          }
+        }
+      }
+    }
+    binaries.executable()
+  }
+
+  sourceSets {
+    commonMain.dependencies {
+      implementation(compose.runtime)
+      implementation(compose.foundation)
+      implementation(compose.material)
+      implementation(compose.ui)
+      implementation(compose.components.resources)
+      implementation(compose.components.uiToolingPreview)
+    }
+  }
+}
+
+compose.experimental {
+  web.application {}
+}
